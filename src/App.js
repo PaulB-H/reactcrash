97c227a408d21ea14944102c7d2fdc51d64e2eef
Paul Bernard-Hall <p.bernardhall@gmail.com>
2020-10-21 09:53:23 -0400

    http=>https for mixed ctnt err

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
       .then(res => this.setState({ todos: res.data }))
   }
 
@@ -47,14 +47,14 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
       .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }))
     
   }
 
   // Add Todo
   addTodo = (title) => {
-    axios.post('http://jsonplaceholder.typicode.com/todos', {
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
     title, 
     completed: false
   })
