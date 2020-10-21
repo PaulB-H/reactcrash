4f30ac95a8a8df03e203f4ef3204d7b786e6bc6f
Paul Bernard-Hall <p.bernardhall@gmail.com>
2020-10-21 10:23:39 -0400

    fixed default checkbox values

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -17,7 +17,7 @@ export class TodoItem extends Component {
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    <input checked={this.props.todo.completed ? 'checked' : ''} type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
                     { title }
                     <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
